288e7f19fa16e05f0acc124bc7725cb1bf0aaa63
Michał Kallas <michael.w.kallas@gmail.com>
2022-10-23 20:47:17 +0200

    Login, registration and flashed messages working

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from operator import index
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, RegistrationForm
@@ -11,16 +10,11 @@ app = Flask(__name__)
 
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
+
+#User database
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,6 +28,14 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get_id(user_id)
+
+#Views
 @app.route("/")
 @app.route("/home")
 def home():
@@ -42,19 +44,31 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            return redirect(url_for("home"))
+        flash("Invalid e-mail adress or password!", "danger")
     return render_template("login.html", title="Login", form=form)
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.create_password_hash(form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created!", "success")
+        return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
-
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
 
