f3291029b0750db569e44336565cd3419cb83c46
Michał Kallas <michael.w.kallas@gmail.com>
2022-10-24 15:25:11 +0200

    Users can now log in properly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user
+from flask_login import LoginManager, UserMixin, login_user, login_required
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import LoginForm, RegistrationForm
@@ -11,29 +11,30 @@ app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config["SECRET_KEY"] = SECRET_KEY
 
+#Login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 #User database
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-class User(UserMixin, db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, index=True)
-    hashed_password = db.Column(db.String(150))
-    date_joined = db.Column(db.DateTime(), default=datetime.now, index=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(150), nullable=False)
+    date_joined = db.Column(db.DateTime(), default=datetime.now, nullable=False)
 
     def create_password_hash(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get_id(user_id)
 
 #Views
 @app.route("/")
@@ -48,7 +49,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for("home"))
+            next = request.args.get("next")
+            return redirect(next or url_for("home"))
         flash("Invalid e-mail adress or password!", "danger")
     return render_template("login.html", title="Login", form=form)
 
@@ -67,6 +69,11 @@ def register():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/tracker")
+@login_required
+def tracker():
+    return "Hello, logged in user!"
 
 
 if __name__ == "__main__":
